19054333d2317b022146f1cc3e01051c9fc72621
Fernando Soares <santosfernando2377@gmail.com>
2022-06-24 05:11:51 -0300

    :sparkles: feat: Foi adicionado o conteúdo dinâmico

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,34 +1,67 @@
+import '../../node_modules/regenerator-runtime/runtime';
+import axios from 'axios';
+
 /**
+ * 
  * Integração com Inshorts
  * Github: https://github.com/cyberboysumanjay/Inshorts-News-API (Documentação)
+ * 
  */
 
-let url = 'https://inshorts.deta.dev/news?category=technology';
-let response
+var Response
 
-var request = new XMLHttpRequest();
-request.open("GET", url, false);
+const news = () => {
+    axios.get('https://inshorts.deta.dev/news?category=technology')
+    .then((response) => {
+        Response = response;
+        dom(Response.data.data, Response.data.success);
+    })
+    .catch((error) => {
+        console.log(error);
+    })
+}
 
-request.onreadystatechange = () => {
-    if(request.readyState == 4 && request.status == 200) {
-        response = JSON.parse(request.response);
-        console.log(response);
+const request = new news;
+
+/**
+ * 
+ * Manipulando os dados
+ * 
+ */
+
+function dom(data, success) {
+    
+    if(success == true) {
+        changeData(data);
+    } else if(success == false) {
+        console.log("Erro ao recuperar as notícias!");
     }
 }
 
-request.send();
+function changeData(data) {
 
+    //console.log(data)
 
+    let change = document.getElementsByClassName('card');
 
-/**
- * 
- * Manipulação dos elementos
- * 
- */
+    for (let index = 0; index < change.length; index++) {
 
-var cards = document.getElementsByClassName('card');
+        for (let index1 = 0; index < 4; index++) {
+            change[index].children[1].children[0].innerHTML = data[index1].title;
+            change[index].children[1].children[1].innerText = data[index1].content;
+            
+        }
 
-for (let index = 0; index < cards.length; index++) {
-    console.log(cards[index].childNodes.values);
-    
+        /*data.forEach(element => {
+            change[0].children[1].children[0].innerHTML = element.title;
+            change[0].children[1].children[1].innerText = element.content;
+        });*/
+
+        //change[index].children[1].children[0].innerHTML = "Teste1";
+        //change[index].children[1].children[1].innerText = "Teste";
+        //change[index].children[1].children[0].innerHTML = "Teste1";
+        //change[index].children[1].children[1].innerText = "Teste";
+    }
 }
+
+changeData()
